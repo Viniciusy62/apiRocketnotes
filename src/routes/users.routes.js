009635ac2abe696a0725ae3802ab23ddfd41effa
Vinicius Andrade <viniciusy62@gmail.com>
2024-04-19 16:51:10 -0300

    Adding version control in Rocket Notes API

---
+++ src/routes/users.routes.js
@@ -0,0 +1,27 @@
+const { Router } = require("express");
+
+const UserController = require("../controllers/UsersController");
+
+const userRoutes = Router();
+
+//Exemplo de uso do middleware
+// function myMiddleware(request, response, next) {
+//   console.log("Você passou pelo Middleware");
+
+//   console.log(request.body)
+
+//   if(!request.body.isAdmin) {
+//     return response.json({ message: "user unauthorized" })
+//   }
+
+//   next()
+// }
+
+// userRoutes.post("/", myMiddleware, userController.create);
+
+const userController = new UserController();
+
+userRoutes.post("/", userController.create);
+userRoutes.put("/:id", userController.update);
+
+module.exports = userRoutes;
